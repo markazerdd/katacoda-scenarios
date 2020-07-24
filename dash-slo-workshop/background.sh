0bb7aa0e4b60343bb6b5fe9431d6638093cb76bc
Mark Azer <mark.azer@datadoghq.com>
2020-07-23 20:49:05 -0400

    Update background script for testing

--- dash-slo-workshop/background.sh
+++ dash-slo-workshop/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /slo-workshop
-git clone https://github.com/DataDog/slo-workshop /slo-workshop
+git clone https://github.com/DataDog/ecommerce-workshop /slo-workshop
 cd /slo-workshop
 docker-compose pull
