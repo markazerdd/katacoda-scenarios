6c608f1d31ab18bbc2240af48f7dc6e815e26fe4
Mark Azer <mark.azer@datadoghq.com>
2020-07-24 08:43:12 -0400

    Try different scripts

--- dash-slo-workshop/background.sh
+++ dash-slo-workshop/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /slo-workshop
-git clone https://github.com/DataDog/ecommerce-workshop /slo-workshop
-cd /slo-workshop
+git clone https://github.com/DataDog/ecommerce-workshop /ecommerce-workshop
+cd /ecommerce-workshop
 docker-compose pull
--- dash-slo-workshop/foreground.sh
+++ dash-slo-workshop/foreground.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=postgres
-cd /slo-workshop
+cd /ecommerce-workshop
 echo Welcome to the Ensuring Reliability with SLOs Training Workshop!
